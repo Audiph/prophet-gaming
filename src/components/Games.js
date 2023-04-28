513ecb43a9dad91fcb5d47dc96b2d184811fe1ac
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-04-29 04:44:32 +0800

    responsive games component

--- src/components/Games.js
+++ src/components/Games.js
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 const Game = ({ name, x, y }) => {
   return (
     <motion.div
-      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-pointer absolute dark:text-dark dark:bg-light"
+      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-pointer absolute dark:text-dark dark:bg-light lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:dark:bg-transparent xs:text-dark xs:dark:text-light xs:font-bold"
       whileHover={{ scale: 1.05 }}
       initial={{ x: 0, y: 0 }}
       whileInView={{ x: x, y: y, transition: { duration: 1.5 } }}
@@ -18,10 +18,13 @@ const Game = ({ name, x, y }) => {
 const Games = () => {
   return (
     <>
-      <h2 className="font-bold text-8xl mt-64 w-full text-center">Games</h2>
-      <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark">
-        <Game name="" x="" y="" />
-        <Game name="Dota 2" />
+      <h2 className="font-bold text-8xl mt-64 w-full text-center md:text-6xl md:mt-32">
+        Games
+      </h2>
+      <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark lg:h-[80vh] sm:h-[60vh] xs:h-[50vh] lg:bg-circularLightLg lg:dark:bg-circularDarkLg md:bg-circularLightMd md:dark:bg-circularDarkMd sm:bg-circularLightSm sm:dark:bg-circularDarkSm">
+        <motion.div className="flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer dark:text-dark dark:bg-light lg:p-6 md:p-4 xs:text-xs xs:p-2">
+          Dota 2
+        </motion.div>
         <Game name="CSGO" x="-20vw" y="2vw" />
         <Game name="Valorant" x="-5vw" y="-10vw" />
         <Game name="Rust" x="20vw" y="6vw" />
