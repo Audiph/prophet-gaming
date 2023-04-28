cd6f1a5b0c98e5fc2bbefbbfdd490b79ea3464e7
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-04-29 06:24:45 +0800

    add responsive to Moderators component

--- src/components/Moderators.js
+++ src/components/Moderators.js
@@ -8,7 +8,7 @@ const Details = ({ name, role, country, details }) => {
   return (
     <li
       ref={ref}
-      className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between"
+      className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]"
     >
       <LiIcon reference={ref} />
       <motion.div
@@ -17,11 +17,13 @@ const Details = ({ name, role, country, details }) => {
         transition={{ duration: 0.5, type: 'spring' }}
       />
       <div>
-        <h3 className="capitalize font-bold text-2xl">{name}</h3>
-        <span className="capitalize font-medium text-dark/75 dark:text-light/75">
+        <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
+          {name}
+        </h3>
+        <span className="capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm">
           {role} | {country}
         </span>
-        <p className="font-medium w-full">{details}</p>
+        <p className="font-medium w-full md:text-sm">{details}</p>
       </div>
     </li>
   );
@@ -37,16 +39,16 @@ const Moderators = () => {
 
   return (
     <div className="my-64">
-      <h2 className="font-bold text-8xl mb-32 w-full text-center">
+      <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
         Moderators
       </h2>
 
-      <div ref={ref} className="w-[75%] mx-auto relative">
+      <div ref={ref} className="w-[75%] mx-auto relative lg:w-[90%] md:w-full">
         <motion.div
           style={{ scaleY: scrollYProgress }}
-          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light"
+          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px] xs:left-[20px]"
         />
-        <ul className="w-full flex flex-col items-start justify-between ml-4">
+        <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
           <Details
             name="Zoro-kol"
             role="Moderator"
