8a9a73f507fddde624dac354efdf0813f97f6e94
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-04-28 19:56:08 +0800

    add responsive home page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,12 @@ import { DiscordIconBNW, FacebookIconBNW, GithubIconBNW } from './Icons';
 
 const Footer = () => {
   return (
-    <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg">
-      <Layout className="py-8 flex items-center justify-between !bg-dark dark:!bg-light">
+    <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg sm:text-base">
+      <Layout className="py-8 flex items-center justify-between !bg-dark dark:!bg-light lg:flex-col lg:py-6">
         <span className="text-white dark:text-black">
           {new Date().getFullYear()} &copy; All Rights Reserved
         </span>
-        <nav className="flex items-center justify-center flex-wrap">
+        <nav className="flex items-center justify-center flex-wrap lg:py-2">
           <motion.a
             href="https://www.facebook.com/propetagaming"
             target={'_blank'}
@@ -18,7 +18,10 @@ const Footer = () => {
             whileTap={{ scale: 0.9 }}
             className="w-6 mx-3"
           >
-            <FacebookIconBNW strokeColor="stroke-white dark:stroke-black" />
+            <FacebookIconBNW
+              className="lg:w-5"
+              strokeColor="stroke-white dark:stroke-black"
+            />
           </motion.a>
           <motion.a
             href="https://discords.com/servers/700876309194080298"
@@ -27,7 +30,10 @@ const Footer = () => {
             whileTap={{ scale: 0.9 }}
             className="w-6 mx-3"
           >
-            <DiscordIconBNW strokeColor="fill-white dark:fill-black" />
+            <DiscordIconBNW
+              className="lg:w-5"
+              strokeColor="fill-white dark:fill-black"
+            />
           </motion.a>
           <motion.a
             href="https://github.com/Audiph"
@@ -36,7 +42,10 @@ const Footer = () => {
             whileTap={{ scale: 0.9 }}
             className="w-6 ml-3"
           >
-            <GithubIconBNW strokeColor="fill-white dark:fill-black" />
+            <GithubIconBNW
+              className="lg:w-5"
+              strokeColor="fill-white dark:fill-black"
+            />
           </motion.a>
         </nav>
       </Layout>
