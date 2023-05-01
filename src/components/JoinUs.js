1903eef7fb0f42bda610033a14ec79a43e968ff5
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-05-02 05:57:08 +0800

    modify articles page to be responsive and tweak home page

--- src/components/JoinUs.js
+++ src/components/JoinUs.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const JoinUs = () => {
   return (
-    <div className="fixed left-4 bottom-20 flex items-center justify-center md:right-1 md:left-auto md:top-20 md:bottom-auto md:absolute">
+    <div className="fixed left-4 bottom-20 flex items-center justify-center md:right-1 md:left-auto md:top-20 md:bottom-auto md:absolute sm:right-0">
       <div className="w-48 h-auto flex items-center justify-center relative md:w-24 md:top-2 md:bottom-auto">
         <Link
           href="https://discord.gg/kJBrUsmdjp"
